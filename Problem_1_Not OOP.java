8cf910514422f0ac30f7f2013417099351e82bf3
mmanoura97 <88887028+mmanoura97@users.noreply.github.com>
2022-10-19 11:29:39 +0300

    Update Problem_1_Not OOP.java

--- Problem_1_Not OOP.java
+++ Problem_1_Not OOP.java
@@ -2,7 +2,7 @@
  
 //Make a program that reads as an input the price, the name and the score of a product. Then it shows as an output the best ratio of the products (score/price) thus the best product also.
 
- import java.util.Scanner; (kanw import ti vivliothiki gia na mporo na xrisimopoiso tin klasi scanner)
+ import java.util.Scanner; 
  
  public class Main {
 	 
