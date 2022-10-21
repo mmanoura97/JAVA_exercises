b743ffd68a9d1ebe138a485f53d1adfaff474fb3
mmanoura97 <mariamanoura997@gmail.com>
2022-10-21 17:17:25 +0300

    adding readme

--- Problem_2/Problem_2_OOP.java
+++ Problem_2/Problem_2_OOP.java
@@ -1,8 +1,3 @@
-//Object Oriented problem on JAVA
- 
-//Make a program that reads as an input the price, the name and the score of a product. Then it shows as an output the best ratio of the products (score/price) thus the best product also.
-//The same problem with Problem_1_Not_OOP but from Object Oriented point of view
- 
 import java.util.Scanner; 
  
 //creating the Product class that includes all the product's attributes
---
+++ Problem_2/README.md
@@ -0,0 +1,4 @@
+//Object Oriented problem on JAVA
+ 
+//Make a program that reads as an input the price, the name and the score of a product. Then it shows as an output the best ratio of the products (score/price) thus the best product also.
+//The same problem with Problem_1_Not_OOP but from Object Oriented point of view
